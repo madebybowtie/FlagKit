231fd049cb04320bdb926b5b58b5d61d21795628
Simon Blommegård <simon@blommegard.se>
2015-10-25 21:16:06 +0100

    1 Workspace -> 1 Project

--- FlagKit-OSX/FlagKit.h
+++ FlagKit-OSX/FlagKit.h
@@ -1,17 +1,17 @@
 //
 //  FlagKit.h
-//  FlagKit
+//  FlagKit-OSX
 //
-//  Created by Simon Blommegård on 21/10/15.
+//  Created by Simon Blommegard on 25/10/15.
 //  Copyright © 2015 Bowtie. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-//! Project version number for FlagKit.
+//! Project version number for FlagKit-OSX.
 FOUNDATION_EXPORT double FlagKitVersionNumber;
 
-//! Project version string for FlagKit.
+//! Project version string for FlagKit-OSX.
 FOUNDATION_EXPORT const unsigned char FlagKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FlagKit/PublicHeader.h>
--- FlagKit-iOS/FlagKit.h
+++ FlagKit-iOS/FlagKit.h
@@ -1,17 +1,17 @@
 //
 //  FlagKit.h
-//  FlagKit
+//  FlagKit-iOS
 //
-//  Created by Simon Blommegård on 21/10/15.
+//  Created by Simon Blommegard on 25/10/15.
 //  Copyright © 2015 Bowtie. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for FlagKit.
+//! Project version number for FlagKit-iOS.
 FOUNDATION_EXPORT double FlagKitVersionNumber;
 
-//! Project version string for FlagKit.
+//! Project version string for FlagKit-iOS.
 FOUNDATION_EXPORT const unsigned char FlagKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FlagKit/PublicHeader.h>
